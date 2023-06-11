ffb6fda6983c6bb82e09b76a17b5f6c6faa0dd9e
karimcherrab <ma.cherrab@gmail.com>
2023-06-11 04:48:00 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import { ContextProvider } from './contexts/ContextProvider';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ 
+  <Router>
+  <AuthProvider>
+  <ContextProvider>
+  <App />
+  </ContextProvider>
+  
+
+  </AuthProvider>
+ 
+</Router>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
